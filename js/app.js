fcfbf7e3ebaa01d3daa3d6cb2cf68a5f4f2b1a6b
Drew Neece <dneece5@aggienetwork.com>
2022-12-19 09:56:23 -0600

    assign cached elements for each pyramid position, and render those cards

--- js/app.js
+++ js/app.js
@@ -12,7 +12,36 @@ let cardToRemove
 
 let stockEl = document.getElementById('stock')
 let wasteEl = document.getElementById('waste')
+
+      // Cached Elements for Each Card in the Pyramid
 let card0El = document.getElementById('p0')
+let card1El = document.getElementById('p1')
+let card2El = document.getElementById('p2')
+let card3El = document.getElementById('p3')
+let card4El = document.getElementById('p4')
+let card5El = document.getElementById('p5')
+let card6El = document.getElementById('p6')
+let card7El = document.getElementById('p7')
+let card8El = document.getElementById('p8')
+let card9El = document.getElementById('p9')
+let card10El = document.getElementById('p10')
+let card11El = document.getElementById('p11')
+let card12El = document.getElementById('p12')
+let card13El = document.getElementById('p13')
+let card14El = document.getElementById('p14')
+let card15El = document.getElementById('p15')
+let card16El = document.getElementById('p16')
+let card17El = document.getElementById('p17')
+let card18El = document.getElementById('p18')
+let card19El = document.getElementById('p19')
+let card20El = document.getElementById('p20')
+let card21El = document.getElementById('p21')
+let card22El = document.getElementById('p22')
+let card23El = document.getElementById('p23')
+let card24El = document.getElementById('p24')
+let card25El = document.getElementById('p25')
+let card26El = document.getElementById('p26')
+let card27El = document.getElementById('p27')
 
 // Event listeners
 
@@ -39,6 +68,63 @@ function pyramidRender() {
     // Pushes that value into pyramid array
     pyramid.push(cardGone)
   }
+  // Hard Code shuffled cards into the Pyramid
+  card0El.classList.add(pyramid[0])
+  card0El.classList.remove('outline')
+  card1El.classList.add(pyramid[1])
+  card1El.classList.remove('outline')
+  card2El.classList.add(pyramid[2])
+  card2El.classList.remove('outline')
+  card3El.classList.add(pyramid[3])
+  card3El.classList.remove('outline')
+  card4El.classList.add(pyramid[4])
+  card4El.classList.remove('outline')
+  card5El.classList.add(pyramid[5])
+  card5El.classList.remove('outline')
+  card6El.classList.add(pyramid[6])
+  card6El.classList.remove('outline')
+  card7El.classList.add(pyramid[7])
+  card7El.classList.remove('outline')
+  card8El.classList.add(pyramid[8])
+  card8El.classList.remove('outline')
+  card9El.classList.add(pyramid[9])
+  card9El.classList.remove('outline')
+  card10El.classList.add(pyramid[10])
+  card10El.classList.remove('outline')
+  card11El.classList.add(pyramid[11])
+  card11El.classList.remove('outline')
+  card12El.classList.add(pyramid[12])
+  card12El.classList.remove('outline')
+  card13El.classList.add(pyramid[13])
+  card13El.classList.remove('outline')
+  card14El.classList.add(pyramid[14])
+  card14El.classList.remove('outline')
+  card15El.classList.add(pyramid[15])
+  card15El.classList.remove('outline')
+  card16El.classList.add(pyramid[16])
+  card16El.classList.remove('outline')
+  card17El.classList.add(pyramid[17])
+  card17El.classList.remove('outline')
+  card18El.classList.add(pyramid[18])
+  card18El.classList.remove('outline')
+  card19El.classList.add(pyramid[19])
+  card19El.classList.remove('outline')
+  card20El.classList.add(pyramid[20])
+  card20El.classList.remove('outline')
+  card21El.classList.add(pyramid[21])
+  card21El.classList.remove('outline')
+  card22El.classList.add(pyramid[22])
+  card22El.classList.remove('outline')
+  card23El.classList.add(pyramid[23])
+  card23El.classList.remove('outline')
+  card24El.classList.add(pyramid[24])
+  card24El.classList.remove('outline')
+  card25El.classList.add(pyramid[25])
+  card25El.classList.remove('outline')
+  card26El.classList.add(pyramid[26])
+  card26El.classList.remove('outline')
+  card27El.classList.add(pyramid[27])
+  card27El.classList.remove('outline')
 }
 
 // Function to handle a button click:
@@ -82,7 +168,6 @@ function render(cardPicked) {
     stockEl.classList.remove('back-red')
   }
 }
-
 
 // Define the required variables to track the state of the game.
   // cardOne: will store the value of the first card selected.
